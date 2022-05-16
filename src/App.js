5b102dfdb355bb02d0b65b6d3cfa75bd391e249d
Abdul-Kadir Coskun <Abdulcoskun@hotmail.com>
2022-05-17 09:28:16 +1000

    Added App.module.scss and added tags to App.js
    Signed-off-by: Abdul-Kadir Coskun <Abdulcoskun@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-function App() {
-    return <div className="App"></div>;
-}
+import React from "react";
+import styles from "./App.module.scss";
+import { Footer } from "./containers/Footer/Footer";
+import { Header } from "./containers/Header/Header";
+import { Main } from "./containers/Main/Main";
+import { Nav } from "./containers/Nav/Nav";
 
-export default App;
+export default function App() {
+    return (
+        <div className={styles.App}>
+            <div className={styles.background}>
+                <div className={styles.twinkling}>
+                    <div className={styles.clouds}>
+                        <Nav />
+                        <Header />
+                        <Main />
+                        <Footer />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
