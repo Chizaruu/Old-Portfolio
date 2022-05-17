9e5c825981fda4ce9a5da33da7ce0f961d9f719b
Abdul-Kadir Coskun <Abdulcoskun@hotmail.com>
2022-05-17 13:34:14 +1000

    Removed unneeded images, Created mainMixins scss, Set up Skills and Projects containers
    Signed-off-by: Abdul-Kadir Coskun <Abdulcoskun@hotmail.com>

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.scss";
 
 export function Footer() {
     return (
-        <footer className={styles}>
+        <footer className={styles.footer}>
             Made with <span title="Latvia!~"> ❤️ </span> by Abdul-Kadir Coskun
         </footer>
     );
---
+++ src/containers/Main/Projects/Projects.jsx
@@ -0,0 +1,176 @@
+import styles from "./Projects.module.scss";
+
+export function Projects() {
+    return (
+        <section className={styles.projects} id="projects">
+            <h2>Projects</h2>
+            <div className={styles.projects__container}>
+                <div className={styles.projects__container__item}>
+                    <div
+                        className={
+                            styles.projects__container__item__info_divider
+                        }
+                    >
+                        <img
+                            className={
+                                styles.projects__container__item__info_divider__image
+                            }
+                            src="images/rlsktd.webp"
+                            alt="Roguelike Starter Kit Top Down"
+                        />
+                        <h3
+                            className={
+                                styles.projects__container__item__info_divider__name
+                            }
+                        >
+                            RLSKTD
+                        </h3>
+                        <p
+                            className={
+                                styles.projects__container__item__info_divider__description
+                            }
+                        >
+                            An open-source framework I started to be a starting
+                            point for future roguelikes.
+                        </p>
+                    </div>
+                    <div
+                        className={
+                            styles.projects__container__item__link_divider
+                        }
+                    >
+                        <a
+                            className={
+                                styles.projects__container__item__link_divider__link
+                            }
+                            href="https://github.com/Chizaruu/rlsktd"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            title="See the code."
+                        >
+                            <img
+                                className={
+                                    (styles.projects__container__item__link_divider__link__icon,
+                                    styles.projects__container__item__link_divider__link__icon_invert)
+                                }
+                                src="images/icons/github.svg"
+                                alt="Github"
+                            />
+                            <p>&lt;Code /&gt;</p>
+                        </a>
+                    </div>
+                </div>
+                <div className={styles.projects__container__item}>
+                    <div
+                        className={
+                            styles.projects__container__item__info_divider
+                        }
+                    >
+                        <img
+                            className={
+                                styles.projects__container__item__info_divider__image
+                            }
+                            src="images/remember.webp"
+                            alt="Remember"
+                        />
+                        <h3
+                            className={
+                                styles.projects__container__item__info_divider__name
+                            }
+                        >
+                            Remember
+                        </h3>
+                        <p
+                            className={
+                                styles.projects__container__item__info_divider__description
+                            }
+                        >
+                            A narrated short game that focuses on a man
+                            reminiscing about his life.
+                        </p>
+                    </div>
+                    <div
+                        className={
+                            styles.projects__container__item__info_divider
+                        }
+                    >
+                        <a
+                            className={
+                                styles.projects__container__item__link_divider__link
+                            }
+                            href="https://chizaruu.github.io/Remember/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            title="Play on GitHub"
+                        >
+                            <img
+                                className={
+                                    (styles.projects__container__item__link_divider__link__icon,
+                                    styles.projects__container__item__link_divider__link__icon_invert)
+                                }
+                                src="images/icons/github.svg"
+                                alt="Github"
+                            />
+                            <p>Play</p>
+                        </a>
+                    </div>
+                </div>
+                <div className={styles.projects__container__item}>
+                    <div
+                        className={
+                            styles.projects__container__item__info_divider
+                        }
+                    >
+                        <img
+                            className={
+                                styles.projects__container__item__info_divider__image
+                            }
+                            src="images/for-the-bros.webp"
+                            alt="For The Bros"
+                        />
+                        <h3
+                            className={
+                                styles.projects__container__item__info_divider__name
+                            }
+                        >
+                            For The Bros
+                        </h3>
+                        <p
+                            className={
+                                styles.projects__container__item__info_divider__description
+                            }
+                        >
+                            A game I made to test my skills in Unity and C# over
+                            a two week period.
+                        </p>
+                    </div>
+                    <div
+                        className={
+                            styles.projects__container__item__info_divider
+                        }
+                    >
+                        <a
+                            className={
+                                styles.projects__container__item__link_divider__link
+                            }
+                            href="https://chizaruu.itch.io/for-the-bros"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            title="Play on itch"
+                        >
+                            <img
+                                className={
+                                    (styles.projects__container__item__link_divider__link__icon,
+                                    styles.projects__container__item__link_divider__link__icon_invert)
+                                }
+                                src="images/icons/itch-io.svg"
+                                alt="Itch"
+                            />
+                            <p>Play</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
